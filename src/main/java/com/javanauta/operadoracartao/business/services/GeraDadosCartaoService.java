1eff98fbebc95341ffd87fd3aaaf805cb30e9daf
Jadeilton <jadeiltondm@gmail.com>
2026-03-28 17:29:18 -0300

    ..

--- src/main/java/com/javanauta/operadoracartao/business/services/GeraDadosCartaoService.java
+++ src/main/java/com/javanauta/operadoracartao/business/services/GeraDadosCartaoService.java
@@ -22,6 +22,7 @@ public class GeraDadosCartaoService {
 
         return new CartaoEntity(numeroCartao, dataExpiracao, cvv, limiteCredito, cliente, cliente.getCartao().getUltimaAlteracaoLimite(),
                 cliente.getCartao().getDataVencimentoFatura());
+
     }
 
     private String gerarNumeroCartao() {
